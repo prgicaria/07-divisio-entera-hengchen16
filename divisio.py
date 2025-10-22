a394afe8fd807126caadd6228f1ec838093a2738
hengchen16 <hchen@instituticaria.cat>
2025-10-22 10:27:41 +0200

    M

--- divisio.py
+++ divisio.py
@@ -2,7 +2,7 @@ dividendo = int(input("Posa el dividend: "))
 divisor = int(input("Posa el divisor: "))
 
 cociente = dividendo // divisor
-residuo = dividendo % divisor
+residuo = dividendo % divisor 
 
 print(f"Divisió: {dividendo} / {divisor}")
 print(f"Quocient: {cociente}")
